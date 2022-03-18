79fff14f0e0203d37789ac4c04d0272fff318c02
artemismars <artemismars@outlook.com>
2022-03-18 22:21:03 +0900

    Update get

--- routes/routers.js
+++ routes/routers.js
@@ -1,4 +1,4 @@
 const router = require("express").Router();
 router.use(require("./user"));
-router.use("/", (req, res) => res.json({ message: "welcome to test page" }));
+router.get("/", (req, res) => res.json({ message: "welcome to test page" }));
 module.exports = router;
